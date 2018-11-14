fcb095f54b514402236a3dae9fe4085260522141
Brady Dean <2bdkid@gmail.com>
2018-11-14 16:39:54 -0500

    heap: generalize heap over any comparison operator

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 #include <vector>
 #include <utility>
 #include <sstream>
+#include <functional>
 
-template<typename Type>
-class max_heap {
+template<typename Type, typename Compare>
+class heap {
 public:
   using value_type = Type;
   using reference = Type&;
@@ -14,12 +15,13 @@ public:
   using const_iterator = typename std::vector<Type>::const_iterator;
   using difference_type = typename std::vector<Type>::difference_type;
   using size_type = typename std::vector<Type>::size_type;
+  using value_compare = Compare;
 
-  max_heap() = default;
-  ~max_heap() = default;
-  max_heap(iterator begin, iterator end);
-  max_heap(const max_heap<Type>& other);
-  max_heap(max_heap<Type>&& other);
+  heap();
+  ~heap() = default;
+  heap(iterator begin, iterator end);
+  heap(const heap<Type, Compare>& other);
+  heap(heap<Type, Compare>&& other);
 
   iterator begin();
   const_iterator begin() const;
@@ -28,13 +30,13 @@ public:
   const_iterator end() const;
   const_iterator cend() const;
 
-  reference operator=(max_heap<Type> other);
-  reference operator=(max_heap<Type>&& other);
-  bool operator==(const max_heap<Type>& other);
-  bool operator!=(const max_heap<Type>& other);
-  void swap(max_heap& other);
-  template<typename T>
-  friend void swap(max_heap<T>& lhs, max_heap<T>& rhs);
+  reference operator=(heap<Type, Compare> other);
+  reference operator=(heap<Type, Compare>&& other);
+  bool operator==(const heap<Type, Compare>& other);
+  bool operator!=(const heap<Type, Compare>& other);
+  void swap(heap& other);
+  template<typename T, typename C>
+  friend void swap(heap<T, C>& lhs, heap<T, C>& rhs);
   size_type size() const;
   size_type max_size() const;
   bool empty() const;
@@ -59,109 +61,113 @@ private:
   
   std::vector<value_type> _rep;
   size_type _size = 0;
+  Compare comp;
 };
 
-template<typename Type>
-max_heap<Type>::max_heap(iterator begin, iterator end)
-  : _rep(begin, end) {
+template<typename Type, typename Compare>
+heap<Type, Compare>::heap() : comp(Compare()) {}
+
+template<typename Type, typename Compare>
+heap<Type, Compare>::heap(iterator begin, iterator end)
+  : _rep(begin, end), comp(Compare()) {
   _size = _rep.size();  
   build();
 }
 
-template<typename Type>
-max_heap<Type>::max_heap(const max_heap<Type>& other)
+template<typename Type, typename Compare>
+heap<Type, Compare>::heap(const heap<Type, Compare>& other)
   : _rep(other._rep), _size(other._size) { }
 
-template<typename Type>
-max_heap<Type>::max_heap(max_heap<Type>&& other) {
+template<typename Type, typename Compare>
+heap<Type, Compare>::heap(heap<Type, Compare>&& other) {
   std::swap(_rep, other._rep);
   std::swap(_size, other._size);
 }
 
-template<typename Type>
-typename max_heap<Type>::iterator
-max_heap<Type>::begin() { return _rep.begin(); }
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::iterator
+heap<Type, Compare>::begin() { return _rep.begin(); }
 
-template<typename Type>
-typename max_heap<Type>::const_iterator
-max_heap<Type>::begin() const { return _rep.begin(); }
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::const_iterator
+heap<Type, Compare>::begin() const { return _rep.begin(); }
 
-template<typename Type>
-typename max_heap<Type>::const_iterator
-max_heap<Type>::cbegin() const { return _rep.begin(); }
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::const_iterator
+heap<Type, Compare>::cbegin() const { return _rep.begin(); }
 
-template<typename Type>
-typename max_heap<Type>::iterator
-max_heap<Type>::end() { return begin() + _size; }
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::iterator
+heap<Type, Compare>::end() { return begin() + _size; }
 
-template<typename Type>
-typename max_heap<Type>::const_iterator
-max_heap<Type>::end() const { return begin() + _size; }
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::const_iterator
+heap<Type, Compare>::end() const { return begin() + _size; }
 
-template<typename Type>
-typename max_heap<Type>::const_iterator
-max_heap<Type>::cend() const { return begin() + _size; }
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::const_iterator
+heap<Type, Compare>::cend() const { return begin() + _size; }
 
-template<typename Type>
-typename max_heap<Type>::reference
-max_heap<Type>::operator=(max_heap<Type> other) {
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::reference
+heap<Type, Compare>::operator=(heap<Type, Compare> other) {
   // copy-swap
   std::swap(_rep, other._rep);
   std::swap(_size, other._size);
   return *this;
 }
 
-template<typename Type>
-typename max_heap<Type>::reference
-max_heap<Type>::operator=(max_heap<Type>&& other) {
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::reference
+heap<Type, Compare>::operator=(heap<Type, Compare>&& other) {
   // copy-swap
   std::swap(_rep, other._rep);
   std::swap(_size, other._size);
   return *this;
 }
 
-template<typename Type>
-bool max_heap<Type>::operator==(const max_heap<Type>& other) {
+template<typename Type, typename Compare>
+bool heap<Type, Compare>::operator==(const heap<Type, Compare>& other) {
   return std::equal(begin(), end(), other.begin(), other.end());
 }
 
-template<typename Type>
-bool max_heap<Type>::operator!=(const max_heap<Type>& other) {
+template<typename Type, typename Compare>
+bool heap<Type, Compare>::operator!=(const heap<Type, Compare>& other) {
   return !operator==(other);
 }
 
-template<typename Type>
-void max_heap<Type>::swap(max_heap<Type>& other) {
+template<typename Type, typename Compare>
+void heap<Type, Compare>::swap(heap<Type, Compare>& other) {
   std::swap(_rep, other._rep);
   std::swap(_size, other._size);
 }
 
-template<typename Type>
-void swap(max_heap<Type>& lhs, max_heap<Type> rhs) {
+template<typename Type, typename Compare>
+void swap(heap<Type, Compare>& lhs, heap<Type, Compare> rhs) {
   lhs.swap(rhs);
 }
 
-template<typename Type>
-typename max_heap<Type>::size_type
-max_heap<Type>::size() const { return _size; }
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::size_type
+heap<Type, Compare>::size() const { return _size; }
 
-template<typename Type>
-typename max_heap<Type>::size_type
-max_heap<Type>::max_size() const { return _rep.max_size(); }
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::size_type
+heap<Type, Compare>::max_size() const { return _rep.max_size(); }
 
-template<typename Type>
-bool max_heap<Type>::empty() const { return _size == 0; }
+template<typename Type, typename Compare>
+bool heap<Type, Compare>::empty() const { return _size == 0; }
 
-template<typename Type>
-void max_heap<Type>::build() {
+template<typename Type, typename Compare>
+void heap<Type, Compare>::build() {
   // skip leaf nodes  
   const auto n = begin() + (size() / 2) - 1;
   for (auto i = n; i >= begin(); --i)
     bubble_down(i);
 }
 
-template<typename Type>
-void max_heap<Type>::sort() {
+template<typename Type, typename Compare>
+void heap<Type, Compare>::sort() {
   auto iter = end() - 1;
 
   while (iter >= begin()) {
@@ -173,16 +179,16 @@ void max_heap<Type>::sort() {
   }
 }
 
-template<typename Type>
-typename max_heap<Type>::reference
-max_heap<Type>::maximum() { return *begin(); }
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::reference
+heap<Type, Compare>::maximum() { return *begin(); }
 
-template<typename Type>
-typename max_heap<Type>::const_reference
-max_heap<Type>::maximum() const { return *begin(); }
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::const_reference
+heap<Type, Compare>::maximum() const { return *begin(); }
 
-template<typename Type>
-void max_heap<Type>::remove(iterator elem) {
+template<typename Type, typename Compare>
+void heap<Type, Compare>::remove(iterator elem) {
   using std::swap;
   swap(*elem, *(end() - 1));
   --_size;
@@ -190,42 +196,42 @@ void max_heap<Type>::remove(iterator elem) {
     bubble_down(begin());
 }
 
-template<typename Type>
-void max_heap<Type>::remove_maximum() {
+template<typename Type, typename Compare>
+void heap<Type, Compare>::remove_maximum() {
   remove(begin());
 }
 
-template<typename Type>
-typename max_heap<Type>::iterator
-max_heap<Type>::parent_of(iterator child) {
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::iterator
+heap<Type, Compare>::parent_of(iterator child) {
   // parent = floor((i - 1) / 2)
   const auto idx = child - begin();
   return begin() + (idx - 1) / 2;
 }
 
-template<typename Type>
-typename max_heap<Type>::iterator
-max_heap<Type>::left_child_of(iterator parent) {
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::iterator
+heap<Type, Compare>::left_child_of(iterator parent) {
   // left_child = 2i + 1
   const auto idx = parent - begin();
   return begin() + (2 * idx) + 1;
 }
 
-template<typename Type>
-typename max_heap<Type>::iterator
-max_heap<Type>::right_child_of(iterator parent) {
+template<typename Type, typename Compare>
+typename heap<Type, Compare>::iterator
+heap<Type, Compare>::right_child_of(iterator parent) {
   // right_child = 2i + 2
   const auto idx = parent - begin();
   return begin() + (2 * idx) + 2;
 }
 
-template<typename Type>
-void max_heap<Type>::bubble_up(iterator elem) {
+template<typename Type, typename Compare>
+void heap<Type, Compare>::bubble_up(iterator elem) {
   auto child = elem;
   auto parent = parent_of(child);
 
   // bubble up
-  while (child != parent && *child > *parent) {
+  while (child != parent && comp(*parent, *child)) {
     using std::swap;
     swap(*child, *parent);
     child = parent;
@@ -233,13 +239,13 @@ void max_heap<Type>::bubble_up(iterator elem) {
   }
 }
 
-template<typename Type>
-void max_heap<Type>::bubble_down(iterator elem) {
+template<typename Type, typename Compare>
+void heap<Type, Compare>::bubble_down(iterator elem) {
   bubble_down(elem, end());
 }
 
-template<typename Type>
-void max_heap<Type>::bubble_down(iterator elem, iterator last) {
+template<typename Type, typename Compare>
+void heap<Type, Compare>::bubble_down(iterator elem, iterator last) {
   auto parent = elem;
   auto left_child = left_child_of(parent);
   auto right_child = right_child_of(parent);
@@ -249,13 +255,13 @@ void max_heap<Type>::bubble_down(iterator elem, iterator last) {
     // find maximum of parent, left_child, right_child
     auto max = parent;
     if (left_child < last)
-      if (*max < *left_child)
+      if (comp(*max, *left_child))
         max = left_child;
     if (right_child < last)
-      if (*max < *right_child)
+      if (comp(*max, *right_child))
         max = right_child;
 
-    // max_heap property fixed
+    // heap property fixed
     if (parent == max) break;
 
     // swap with the greater child
@@ -267,8 +273,8 @@ void max_heap<Type>::bubble_down(iterator elem, iterator last) {
   }
 }
 
-template<typename Type>
-void max_heap<Type>::insert(value_type value) {
+template<typename Type, typename Compare>
+void heap<Type, Compare>::insert(value_type value) {
   if (_size < _rep.size()) {
     _rep[_size++] = std::move(value);
   } else {
@@ -279,8 +285,8 @@ void max_heap<Type>::insert(value_type value) {
   bubble_up(end() - 1);
 }
 
-template<typename Type>
-std::ostream& operator<<(std::ostream& out, const max_heap<Type>& heap) {
+template<typename Type, typename Compare>
+std::ostream& operator<<(std::ostream& out, const heap<Type, Compare>& heap) {
   // output contents of heap
   if (heap.size() > 0) {
     std::cout << *heap.begin();
@@ -290,6 +296,12 @@ std::ostream& operator<<(std::ostream& out, const max_heap<Type>& heap) {
   
   return out;
 }
+
+template<typename Type>
+using max_heap = heap<Type, std::less<Type>>;
+
+template<typename Type>
+using min_heap = heap<Type, std::greater<Type>>;
 
 int main() {
   std::string line;
@@ -314,7 +326,7 @@ int main() {
     }
   }
 
-  // linear time to build max_heap
+  // linear time to build heap
   max_heap<int> h(arr.begin(), arr.end());
   std::cout << "h before: " << h << '\n';
   h.sort();
